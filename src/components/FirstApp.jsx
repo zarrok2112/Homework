49b424df9075543b50e1cac8b9225ca557664fdd
zarrok <anakinvv@gmail.com>
2023-02-24 00:55:37 -0500

    challenge 2 1. Create a new FirstApp Component 2. Return a new title into h1 tag and a text “10” into one span tag 3. Import and Deploy FirstApp

--- src/components/FirstApp.jsx
+++ src/components/FirstApp.jsx
@@ -2,34 +2,12 @@ import { useState } from 'react';
 import reactLogo from '../assets/logo.gif'
 import '../App.css'
 
-export const FirstApp = ({value = 100}) => {
-    const [counter, setCounter] = useState(value)
-    
-    const handleAdd = () => {
-        setCounter(counter + 1)
-    }
-
-    const handleSubstract = () =>{
-        setCounter(counter - 1)
-    }
-
-    const handlereset = () =>{
-        setCounter(value)
-    }
+export const FirstApp = () => {
 
     return(
         <>
-                  <img src={reactLogo} className="logo react" alt="React logo" />
-        <h1>
-        <span> {counter} </span>
-        
-        </h1>
-        <h2>
-            <button onClick={() => handleAdd()}>  +1</button>
-            <button onClick={() => handlereset()}> reset </button>
-            <button onClick={() => handleSubstract()} >-1</button>
-        </h2>
-        
-        </>
+        <h1>10</h1>
+        <span>Hola mundo!!</span>
+        </>       
     )
 }
