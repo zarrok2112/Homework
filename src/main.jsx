80acbf40f0ce81da0187185ecdfc58dc58702878
zarrok <anakinvv@gmail.com>
2023-02-24 01:09:46 -0500

    challenge 4 1. Add a new input tag to write some text, use onChange event to get changes on input 2. Handle above input with useState hook to setCategory 3. Send event variable to setCategory function to get value from input 4. Add a new Button to add categories 5. Use useState hook to addCategories to the list with onClick event button. 6. Inside above item, call setCategory to clear input field after add one category

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Cards from './components/Cards';
+import {ComponentApp} from './components/.';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css/animate.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <ComponentApp/>
   </React.StrictMode>,
 )
