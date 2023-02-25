983f4ed48ca16cbaf6b28a3c60a9db2f39e12309
zarrok <anakinvv@gmail.com>
2023-02-24 20:05:22 -0500

    Challenge 07 1. Create a new customHook called useFetch 2. Implements fetch api which receives any url and returns data from fetch when it changes , as follow: { data: null, isLoading : hasError : null } * Use needed Hooks to make it works

---
+++ src/components/fetch.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useFetch } from "../hooks/useFetch";
+
+export const Fetch = () => {
+  const url = "https://api.gameofthronesquotes.xyz/v1/random";
+  const { data, isLoading, hasError } = useFetch(url);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (hasError) {
+    return <p>Error: {hasError.message}</p>;
+  }
+
+  // Recorrer las propiedades del objeto data con un bucle for...in
+  for (const key in data) {
+    // Imprimir el nombre de cada propiedad y su valor correspondiente
+    console.log(`${key}:`, data[key]);
+  }
+
+  return null;
+};
+
+export default Fetch;
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {FirstApp} from './components/FirstApp';
+import {Fetch} from './components/fetch';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'animate.css/animate.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FirstApp/>
+    <Fetch/>
   </React.StrictMode>,
 )
